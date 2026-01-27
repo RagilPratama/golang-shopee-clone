7017e81008afe8c423672c32535995794cd1010c
ragilpratama <ragilpratamaaa@gmail.com>
2026-01-27 15:13:49 +0700

    Cors:

--- routes/routes.go
+++ routes/routes.go
@@ -12,6 +12,21 @@ import (
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	})
+
 	r.GET("/products", controllers.FindProducts)
 	r.POST("/products", controllers.CreateProduct)
 	r.GET("/products/:id", controllers.FindProduct)
