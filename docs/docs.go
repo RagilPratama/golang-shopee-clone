c98eab47af7079c0134199d68cce6d74a38adca2
ragilpratama <ragilpratamaaa@gmail.com>
2026-01-27 11:25:20 +0700

    Golang API Update Koneksi

--- docs/docs.go
+++ docs/docs.go
@@ -274,9 +274,9 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "golang-shopee-clone-production.up.railway.app",
 	BasePath:         "/",
-	Schemes:          []string{},
+	Schemes:          []string{"https"},
 	Title:            "Golang Shopee API",
 	Description:      "This is a sample server for a Shopee-like API.",
 	InfoInstanceName: "swagger",
